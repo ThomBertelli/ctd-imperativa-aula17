719da682a7f0e606a2ca621aec3f747503cae73c
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2021-09-23 19:08:28 -0300

    Mesa de Trabalho concluída

--- mesa-de-trabalho/index.js
+++ mesa-de-trabalho/index.js
@@ -1,22 +1,22 @@
 /*
     Atividade I: Inicialize um projeto npm;
-    (  ) Preencha nome do projeto;
-    (  ) Preencha o nome de todos os integrantes do grupo;
-    (  ) Preencha a licença como UNLICENSED;
-    (  ) Preencha a versão como 1.0.
+    (x) Preencha nome do projeto;
+    (x) Preencha o nome de todos os integrantes do grupo;
+    (x) Preencha a licença como UNLICENSED;
+    (x) Preencha a versão como 1.0.
 
     Atividade II: Instale o Nodemon globalmente em sua máquina. Link com a instrução de instalação: https://www.npmjs.com/package/nodemon.
     
     Atividade III: Ignore a pasta node_modules
-    (  ) Configure o git, utilizando o arquivo .gitignore, para que ele ignore as alterações realizas na pasta node_modules.
+    (x) Configure o git, utilizando o arquivo .gitignore, para que ele ignore as alterações realizas na pasta node_modules.
 
     Atividade IV: Crie o projeto 
-    (  ) Instale o Express no projeto (https://expressjs.com/pt-br/),
-    (  ) Implemente a aplicação como as seguintes rotas:
-        (  ) Rota http://localhost:1234/
-            (  ) Insira o texto: Seja bem-vindo ao [nome_do_projeto].
-        (  ) Rota http://localhost:1234/sobre/
-            (  ) Insire o texto: Sobre o projeto
+    (x) Instale o Express no projeto (https://expressjs.com/pt-br/),
+    (x) Implemente a aplicação como as seguintes rotas:
+        (x) Rota http://localhost:1234/
+            (x) Insira o texto: Seja bem-vindo ao [nome_do_projeto].
+        (x) Rota http://localhost:1234/sobre/
+            (x) Insire o texto: Sobre o projeto
 
 
     Comandos npm
@@ -26,3 +26,17 @@
         npm uninstall [nome_do_modulo] -> para desinstalar um módulo de terceiros.
         npm start -> para executar o projeto. 
 */
+
+const express = require ('express')
+
+const aplicativo = new express();
+
+aplicativo.get('/', function(requisicaoAoServidor, respostaDoServidor){
+    respostaDoServidor.send('Seja bem-vindo a Mesa de Trabalho da Aula 17 de Imperativa!');
+});
+
+aplicativo.get('/sobre', function(requisicaoAoServidor, respostaDoServidor){
+    respostaDoServidor.send('Esta é a mesa de trabalho do aluno Thomas Breno. Nessa aula aprendemos sobre instalar módulos via NPM, configurar o Gitignore e usar o express.');
+});
+
+aplicativo.listen(1234);
